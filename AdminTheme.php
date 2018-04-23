d508c004ca799b4b332f4dd8ec8b25ed10911e48
shrouds <game.dev.servers@gmail.com>
2018-04-23 14:07:12 +0300

    Fix #3
    Auto Load Theme

--- AdminTheme.php
+++ AdminTheme.php
@@ -8,16 +8,53 @@
 
 namespace shrouds\admin;
 
-
+use Yii;
 use yii\base\Theme;
+use yii\helpers\FileHelper;
 
 class AdminTheme extends Theme
 {
+    const THEME_NAME = 'adminLte';
+
+    private $themeDirectory;
+
+    /**
+     *
+     * @throws \yii\base\Exception
+     */
     public function init()
     {
-        //parent::init();
-        $this->basePath = __DIR__;
+        $this->themeDirectory = Yii::getAlias( '@app' ).'/themes/'.self::THEME_NAME;
+        $this->setupDirectoryTheme();
+        $this->basePath = $this->themeDirectory;
         $this->baseUrl = '@web';
+        $this->pathMap = [
+            '@app/views' => $this->themeDirectory.'/views',
+            '@app/modules' => $this->themeDirectory.'/modules'
+        ];
+    }
+
+    /**
+     *
+     * @throws \yii\base\Exception
+     */
+    protected function setupDirectoryTheme()
+    {
+        if(!is_dir($this->themeDirectory)){
+            if(FileHelper::createDirectory($this->themeDirectory)){
+                FileHelper::createDirectory($this->themeDirectory.'/views');
+                FileHelper::createDirectory($this->themeDirectory.'/modules');
+                FileHelper::copyDirectory(__DIR__.'/views', $this->themeDirectory.'/views');
+                if(is_dir(Yii::getAlias( '@app' ).'/views/site')) {
+                    FileHelper::copyDirectory(Yii::getAlias('@app') . '/views/site', $this->themeDirectory . '/views/site');
+                }
+            }
+        }
+    }
+
+    public function getThemeDirectory()
+    {
+        return $this->themeDirectory;
     }
 
 }
